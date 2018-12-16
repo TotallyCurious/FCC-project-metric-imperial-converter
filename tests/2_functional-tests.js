56c633451d6d0094d550a3e0bd7eaa64bf4202ef
Glitch (ablaze-coriander) <none>
2018-12-16 08:39:04 +0000

    🍩🛋 Checkpoint ./tests/2_functional-tests.js:2018030/166

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -39,7 +39,6 @@ suite('Functional Tests', function() {
         .query({input: '32g'})
         .end(function(err, res){
           assert.equal(res.status, 200);
-         //TODO needs review
           assert.equal(res.text,'invalid unit');
           done();
         });
@@ -51,7 +50,6 @@ suite('Functional Tests', function() {
         .query({input: '3/7.2/4kg'})
         .end(function(err, res){
           assert.equal(res.status, 200);
-         //TODO needs review
           assert.equal(res.text,'invalid number');
           done();
         });
@@ -63,7 +61,6 @@ suite('Functional Tests', function() {
         .query({input: '3/7.2/4kilomegagram'})
         .end(function(err, res){
           assert.equal(res.status, 200);
-         //TODO needs review
           assert.equal(res.text,'invalid number and unit');
           done();
         });
@@ -75,10 +72,10 @@ suite('Functional Tests', function() {
         .query({input: 'kg'})
         .end(function(err, res){
           assert.equal(res.status, 200);
-          assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
-          assert.approximately(res.body.returnNum, 2.64172, 0.1);
-          assert.equal(res.body.returnUnit, 'gal');
+          assert.equal(res.body.initNum, 1);
+          assert.equal(res.body.initUnit, 'kg');
+          assert.approximately(res.body.returnNum, 2.20462,0.1);
+          assert.equal(res.body.returnUnit, 'lbs');
           done();
         });
       });
