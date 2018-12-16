68b6efbd9d5164816baf6c36249c45c9fe7762d9
Glitch (ablaze-coriander) <none>
2018-12-16 08:28:04 +0000

    🗻🌶 Checkpoint ./tests/2_functional-tests.js:2018030/1259

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -40,27 +40,47 @@ suite('Functional Tests', function() {
         .end(function(err, res){
           assert.equal(res.status, 200);
          //TODO needs review
-          assert.equal(res.body.initNum, 32);
-          assert.equal(res.body.initUnit, 'g');
-          assert.approximately(res.body.returnNum, 2.64172, 0.1);
-          assert.equal(res.body.returnUnit, 'gal');
+          assert.equal(res.text,'invalid unit');
           done();
         });
       });
       
       test('Convert 3/7.2/4kg (invalid number)', function(done) {
-        
-        //done();
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: '3/7.2/4kg'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+         //TODO needs review
+          assert.equal(res.text,'invalid number');
+          done();
+        });
       });  
       
       test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
-        
-        //done();
+      chai.request(server)
+        .get('/api/convert')
+        .query({input: '3/7.2/4kilomegagram'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+         //TODO needs review
+          assert.equal(res.text,'invalid number and unit');
+          done();
+        });
       });
       
       test('Convert kg (no number)', function(done) {
-        
-        //done();
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: 'kg'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 10);
+          assert.equal(res.body.initUnit, 'L');
+          assert.approximately(res.body.returnNum, 2.64172, 0.1);
+          assert.equal(res.body.returnUnit, 'gal');
+          done();
+        });
       });
       
     });
